c51b03a47e733ca78b6a73841331a9b386dac264
Aaron <aaronjerez1@gmail.com>
2022-06-14 23:13:12 -0400

    catch up

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,14 @@
+import {Recipe} from './recipe.model';
+
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('A test recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/d/de/Backzutaten_041.jpg'),
+        new Recipe('Another test recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/McD-McChicken_%28infobox%29.png/2560px-McD-McChicken_%28infobox%29.png'),
+    ];
+
+    getRecipes() {
+        return this.recipes.slice(); //will return a COPY of the array recipes not the reference of it.
+    }
+
+}
